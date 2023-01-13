0409604ca273b286c31a001c96915097ca064bdc
sairashaheen001 <122490088+sairashaheen001@users.noreply.github.com>
2023-01-14 02:00:20 +0500

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,18 +1,13 @@
-// Step 1: Import React
-import * as React from 'react'
 
-// Step 2: Define your component
+import * as React from 'react'
+//component
 const IndexPage = () => {
   return (
     <main>
       <h1>Welcome to my Gatsby site!</h1>
-      <p>I'm making this by following the Gatsby Tutorial.</p>
     </main>
   )
 }
 
-// You'll learn about this in the next task, just copy it for now
-export const Head = () => <title>Home Page</title>
-
-// Step 3: Export your component
 export default IndexPage
+
